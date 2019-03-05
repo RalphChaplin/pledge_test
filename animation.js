8330ac08ff86a034510d8be15878f8a027b4af40
RalphChaplin <ralph.003@hotmail.com>
2019-03-04 20:56:51 -0600

    added number count up

--- animation.js
+++ animation.js
@@ -1,1 +1,11 @@
+let bigGreenNum = document.getElementById('bigGreen');
 
+let i = 0
+
+let intervalId = setInterval(function(){
+  let timeoutId = setTimeout(function(){
+    if (i < 1424) {
+      bigGreenNum.innerText = i++;
+    }
+  }, 100);
+});
